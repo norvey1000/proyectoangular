7bb486476953643b023fa3d925d2b29de56c68f3
norvey1000 <neramirez@uniguajira.edu.co>
2022-03-22 22:40:33 -0500

    pagina contactanos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'inicio',
     component: InicioComponent,
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent,
   }
 ];
 
